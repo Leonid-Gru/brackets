1d467ad9f0262db18a7caf79ae8a36a5d43fbd0e
Леонид <gr.leoneed@gmail.com>
2022-08-12 16:53:38 +0300

    feat: solve task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let stack = [];
+    let bracketsPair = {};
+
+    bracketsConfig.forEach(brackets => {
+        bracketsPair[brackets[0]] = brackets[1];
+    });
+
+    for (let i = 0; i < str.length; i++) {
+        let currentElem = str[i];
+        let topElem = stack[stack.length - 1];
+
+        if (currentElem in bracketsPair) {
+            if (currentElem === bracketsPair[currentElem] && currentElem === topElem) {
+                stack.pop();
+            } else {
+                stack.push(currentElem);
+            }
+        } else {
+            if (stack.length === 0) {
+                return false;
+            }
+            if (bracketsPair[topElem] === currentElem) {
+                stack.pop();
+            } else {
+                return false;
+            }
+        }
+    }
+    return stack.length === 0;
 }
